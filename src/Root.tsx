3a81a4f9c97b2e6672adf13016591a4437b03ffd
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-03 13:05:24 +0900

    最終的なテトリミノグリッドの状態を静止画として描画

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import { Composition } from "remotion"
-import { AutoTetris2d } from "./AutoTetris2d"
+import { LastTetrisGrid } from "./AutoTetris2d/LastTetrisGrid"
 
 // Welcome to the Remotion Three Starter Kit!
 // Two compositions have been created, showing how to use
@@ -19,7 +19,7 @@ import { AutoTetris2d } from "./AutoTetris2d"
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      <Composition id="MyComp" component={AutoTetris2d} durationInFrames={150} fps={30} width={1920} height={1080} />
+      <Composition id="MyComp" component={LastTetrisGrid} durationInFrames={150} fps={30} width={1920} height={1080} />
     </>
   )
 }
